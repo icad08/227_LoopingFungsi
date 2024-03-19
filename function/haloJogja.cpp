d32c251498788c86c5a33520d68f582e40bc72da
icad08 <irsyadrm08@gmail.com>
2024-03-19 15:06:53 +0700

    do while

---
+++ function/haloJogja.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string pilihan; 
+    do
+    {
+        cout << "makan" << endl;
+        cout << "main game" << endl;
+        cout << "ibadah" << endl;
+
+        cout << "apakah anda masih hidup? (yes/no)";
+        cin >> pilihan ;
+    } while (pilihan == "yes");
+}
