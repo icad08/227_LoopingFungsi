0af7dae6757d3191c9ff027f4ea9416aea939458
icad08 <irsyadrm08@gmail.com>
2024-03-19 14:47:47 +0700

    commit 1

---
+++ function/fungtion.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+ {
+    int i; string nama; 
+    for (i = 7; i < 0; i--)
+     {
+        cout << "icadboy" << endl;
+    }
+
+    for (i = 0; i < 5; i++) 
+    { 
+        cout << "masukkan nama =" ;
+        cin >> nama[i];
+
+    }
+     cout << endl;
+        cout << "=========" << endl;
+        cout << "kumpulan nama - nama" << endl;
+
+
+    for ( i=0; i<5;i++) {
+       
+    }
+ }
