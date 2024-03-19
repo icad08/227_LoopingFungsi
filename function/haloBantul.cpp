b14a66a96170a49b21dfcbf144d5d6f1db292a92
icad08 <irsyadrm08@gmail.com>
2024-03-19 15:37:53 +0700

    procedure and function

---
+++ function/haloBantul.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+string nama;
+int bAdidas, bPuma , bNike, bCommpass, bNB;
+int hAdibas = 300,  hNB = 250, hPuma = 150, hNike = 350;
+
+void input()
+{
+cout  << "Masukkan jumlah adidas = ";
+cin >>   nAdidas;
+
+cout << "masukkan jumlah puma = ";
+cin >> nPuma 
+
+cout << "masukkan jumlah New Balance = ";
+cin >> nNB;
+
+cout << "masukkan jumlah compass  = ";
+cin >> nCompass
+
+cout << "masukkan jumlah nike  = ";
+cin >>  nNike;
+}
+
+
+int totalHarga()
+{
+return (nAdidas * hAdibas) + (nPuma * hPuma) + (nNB  - hNB) + (nCompass - hCompass) + (nNike - hNike);
+}
+
+
+    void display()
+
+    cout << endl;
+    cout << "============" << endl;
+    cout << endl; 
+    cout << "jumlah puma =" nPuma << endl;
+    cout << "Jumlah Adidas = " << nAdidas << endl;
+    cout << endl;
+    cout << "Total Harga = Rp." << totalHarga(); << endl;
+   
+
+
+int main ()
+{
+    input();
+    display();
+}
